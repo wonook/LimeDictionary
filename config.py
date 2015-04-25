82fb62af683279ea663ccf9a4f3d248585da6322
MerHS <starvessel@naver.com>
2015-04-25 19:40:20 +0900

    migrate to linux

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__name__))
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = 'mysql+oursql://lime:amdTjnRkqhgakd@localhost/lime'
+SQLALCHEMY_DATABASE_URI = 'mysql+oursql://root:amdTjnRkqhgakd@localhost/lime'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 WTF_CSRF_ENABLED = True
