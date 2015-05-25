77d6310d9597afcf3da15b1932a829c4e4e162c2
KINETC <starvessel@naver.com>
2015-05-26 03:43:25 +0900

    adding json

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 #!flask/bin/python
+import os
 from app import app
-app.run(debug=True, host='0.0.0.0')
+
+
+def runserver():
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+    runserver()
