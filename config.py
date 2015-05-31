5994d83e4df07b69073743f69c839410ba5e5635
Won Wook SONG <wsong0512@gmail.com>
2015-06-01 00:45:10 +0900

    use kinetc database

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__name__))
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = 'mysql+oursql://lime:limelime@localhost/lime?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+oursql://lime:limelime@www.kinetc.net/lime?charset=utf8'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 REDIS_HOST = 'www.kinetc.net'
