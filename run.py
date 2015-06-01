2e95d7c54a5260fe9a8f918d19cf751ae4741959
KINETC <starvessel@naver.com>
2015-06-01 18:10:18 +0900

    port change

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from app import app
 
 
 def runserver():
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 3000))
     app.run(debug=True, host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
